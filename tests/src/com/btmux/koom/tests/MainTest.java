da200797629bc20be73190f3f6710bbce89f599c
cu5@feem.net <cu5@feem.net@152ba900-739f-11de-a9df-91707ec8e704>
2009-07-18 16:00:58 +0000

    Skeleton Eclipse project to start work.

---
+++ tests/src/com/btmux/koom/tests/MainTest.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2009 Timothy Krajcar.
+ *
+ * This file is part of Koom, a BattleTech MUX graphical HUD client.
+ *
+ * Koom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Koom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Koom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.btmux.koom.tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        fail("Not yet implemented");
+    }
+
+}
